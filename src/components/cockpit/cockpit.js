d1c7c6b18143269c3dea9495b206a554bccd58ff
Saravanamani Madasamy <madasamys@Temp13.ad.local>
2020-05-09 18:20:42 +0530

    Component Lif cycle/hooks and functions level hooks

--- src/components/cockpit/cockpit.js
+++ src/components/cockpit/cockpit.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Classes from './cockpit.module.css';
 
-const cockpit = (props) => {
+const Cockpit = (props) => {
+
+    useEffect( () => {
+        console.log("Inside UseEffect funtion Hooks");
+
+        setTimeout(() => {
+            console.log("Inside timeout user effect 1");
+        }, 3000);
+
+        return () => {
+            console.log("Cockpit Removed 1st use Effect");
+        }
+    }, [props.persons]);
+
+
+    useEffect(() => {
+        console.log("Inside 2nd useEffect");
+        alert(2);
+    }, [])
     let classesArr = [Classes.bold];
-    if(props.persons.length > 1){
+    if(props.personsLength > 1){
         classesArr.push(Classes.red);
     } else {
         classesArr.push(Classes.green);
@@ -19,4 +37,4 @@ const cockpit = (props) => {
     )
 }
 
-export default cockpit;
+export default React.memo(Cockpit);
